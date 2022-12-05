cb9ac583d0be7a565a4966934bb876112a504fd6
Q-hj <1485642186@qq.com>
2022-12-05 15:54:35 +0800

    :lipstick:

--- src/pages/error/404.vue
+++ src/pages/error/404.vue
@@ -1,14 +1,14 @@
 <!--
  * @Date: 2022-12-05 15:38:36
  * @LastEditors: Mr.qin
- * @LastEditTime: 2022-12-05 15:51:28
+ * @LastEditTime: 2022-12-05 15:53:58
  * @Description: 404页面
 -->
 <script setup lang="ts">
 	const { back } = useRouter();
 </script>
 <template>
-	<div class="full-screen flex-center">
+	<div class="full-screen flex-center pb-10vh">
 		<a-result
 			status="404"
 			subtitle="你访问的页面不存在！"
