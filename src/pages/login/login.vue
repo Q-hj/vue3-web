6c04d4f018b6dde5576caf6ab7f165de9b54d2fc
Q-hj <1485642186@qq.com>
2022-12-26 10:26:10 +0800

    :art:

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -2,61 +2,61 @@
  * @Date: 2022-11-30 11:35:18
  * @LastEditors: Mr.qin
  * @LastEditTime: 2022-12-05 15:01:03
- * @Description: 
+ * @Description:
 -->
 <script lang="ts" setup>
-	const { push } = useRouter();
+const { push } = useRouter();
 
-	const { DEV } = import.meta.env;
+const { DEV } = import.meta.env;
 
-	// 微信登录配置
-	const config = {
-		appid: 'wx205cd1187819ea10',
-		scope: 'snsapi_login',
-		redirect_uri: encodeURIComponent('http://www.lianchengtech.com' || window.location.origin),
-		state: Math.ceil(Math.random() * 1000),
-		response_type: 'code',
-	};
+// 微信登录配置
+const config = {
+  appid: 'wx205cd1187819ea10',
+  scope: 'snsapi_login',
+  redirect_uri: encodeURIComponent('http://www.lianchengtech.com' || window.location.origin),
+  state: Math.ceil(Math.random() * 1000),
+  response_type: 'code',
+};
 
-	onMounted(() => {
-		if (window.location.search) {
-			// 已登录:-> 获取code
-			const code = window.location.search
-				.slice(1)
-				.split('&')
-				.filter((query) => query.includes('code'));
-			if (code[0]) getUserInfo(code);
-			else handleWxLogin(config);
-		} else {
-			// 未登录:-> 进行微信扫码
-			handleWxLogin(config);
-		}
-	});
+onMounted(() => {
+  if (window.location.search) {
+    // 已登录:-> 获取code
+    const code = window.location.search
+      .slice(1)
+      .split('&')
+      .filter((query) => query.includes('code'));
+    if (code[0]) getUserInfo(code);
+    else handleWxLogin(config);
+  } else {
+    // 未登录:-> 进行微信扫码
+    handleWxLogin(config);
+  }
+});
 
-	// 方式1：将二维码生成在dom上
-	function handleWxLogin(config) {
-		new WxLogin({
-			id: 'wxCode',
-			style: 'black',
-			...config,
-		});
-	}
+// 方式1：将二维码生成在dom上
+function handleWxLogin(config) {
+  new WxLogin({
+    id: 'wxCode',
+    style: 'black',
+    ...config,
+  });
+}
 
-	// 方式2：在新页面进行扫码
-	function handleWxLogin2(config) {
-		const wxLoginUrl = 'https://open.weixin.qq.com/connect/qrconnect';
-		const params = Object.entries(config)
-			.map((item) => item.join('='))
-			.join('&');
+// 方式2：在新页面进行扫码
+function handleWxLogin2(config) {
+  const wxLoginUrl = 'https://open.weixin.qq.com/connect/qrconnect';
+  const params = Object.entries(config)
+    .map((item) => item.join('='))
+    .join('&');
 
-		const loginUrl = wxLoginUrl + '?' + params;
-		window.open(loginUrl);
-	}
+  const loginUrl = wxLoginUrl + '?' + params;
+  window.open(loginUrl);
+}
 
-	function getUserInfo(code) {
-		console.log('code:' + code);
-		//   push('/index');
-	}
+function getUserInfo(code) {
+  console.log('code:' + code);
+  //   push('/index');
+}
 </script>
 
 <template>
@@ -70,4 +70,6 @@
 	</section>
 </template>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,67 +4,71 @@
  * @LastEditTime: 2022-12-05 15:45:27
  * @Description: 路由配置
  */
-import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router';
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
 
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 NProgress.configure({ showSpinner: false });
 
 // 引入展馆的所有页面
-const venuePages = import.meta.glob('@/pages/venue/*.vue');
+const venuePages = import.meta.glob("@/pages/venue/*.vue");
 
 const venueRoutes = Object.values(venuePages).map((importFn) => ({
-	path: '/' + importFn.name.split('/').at(-1).split('.')[0],
-	component: importFn,
+  path: "/" + importFn.name.split("/").at(-1).split(".")[0],
+  component: importFn,
 }));
 
 const router = createRouter({
-	history: createWebHashHistory(),
-	routes: [
-		{
-			path: '/layout',
-			name: '数字展馆',
-			component: () => import('@/pages/layout/layout.vue'),
-			redirect: '/index',
-			children: venueRoutes,
-		},
-		{
-			path: '/login',
-			name: '登录',
-			component: () => import('@/pages/login/login.vue'),
-		},
-		{
-			path: '/404',
-			name: '404',
-			component: () => import('@/pages/error/404.vue'),
-		},
-		{
-			path: '/:pathMatch(.*)*',
-			redirect: '/404',
-		},
-	],
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: "/layout",
+      name: "数字展馆",
+      component: () => import("@/pages/layout/layout.vue"),
+      redirect: "/index",
+      children: venueRoutes,
+    },
+    {
+      path: "/login",
+      name: "登录",
+      component: () => import("@/pages/login/login.vue"),
+    },
+    {
+      path: "/404",
+      name: "404",
+      component: () => import("@/pages/error/404.vue"),
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      redirect: "/login",
+    },
+  ],
 });
 
 //路由跳转开始
 router.beforeEach((to: any, from) => {
-	NProgress.start();
-	// instead of having to check every route record with
-	// to.matched.some(record => record.meta.requiresAuth)
-	//  !auth.isLoggedIn()
-	return;
-	if (to.meta.requiresAuth) {
-		// this route requires auth, check if logged in
-		// if not, redirect to login page.
-		return {
-			path: '/login',
-			// save the location we were at to come back later
-			query: { redirect: to.fullPath },
-		};
-	}
+  NProgress.start();
+  // instead of having to check every route record with
+  // to.matched.some(record => record.meta.requiresAuth)
+  //  !auth.isLoggedIn()
+  return;
+  if (to.meta.requiresAuth) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    return {
+      path: "/login",
+      // save the location we were at to come back later
+      query: { redirect: to.fullPath },
+    };
+  }
 });
 //路由跳转结束
 router.afterEach((to: any, from) => {
-	if (to.name) document.title = to.name;
-	NProgress.done(); // finish progress bar
+  if (to.name) document.title = to.name;
+  NProgress.done(); // finish progress bar
 });
 export default router;
