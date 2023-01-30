67b6c003f600cba2a683e4c1c0a7d9e6ce2da09b
Q-hj <1485642186@qq.com>
2023-01-30 10:17:59 +0800

    :art:新增全局声明

--- src/hooks/useChangeIndex.js
+++ src/hooks/useChangeIndex.js
@@ -2,18 +2,18 @@
  * @Date: 2022-08-17 17:10:12
  * @LastEditors: Mr.qin
  * @LastEditTime: 2022-08-24 11:07:03
- * @Description: 异步加载
+ * @Description:递减
  */
-import { ref, computed } from 'vue';
+import { ref, computed } from "vue";
 export default function useChangeIndex() {
-	const changIndex = ref(0);
+  const changIndex = ref(0);
 
-	function pre() {
-		changIndex.value--;
-		if (!changIndex.value) return;
-	}
-	return {
-		changIndex: computed(() => changIndex.value),
-		pre,
-	};
+  function pre() {
+    changIndex.value--;
+    if (!changIndex.value) return;
+  }
+  return {
+    changIndex: computed(() => changIndex.value),
+    pre,
+  };
 }
